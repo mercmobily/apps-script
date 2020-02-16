325350f2ac08c352ad97ad1fc2f1449e2b8f4af9
Tony Mobily <tonymobily@gmail.com>
2020-02-16 11:29:14 +0800

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,23 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
+//import { createDefaultConfig } from '@open-wc/building-rollup';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './index.html' });
+//export default createDefaultConfig({ input: './index.html' });
+// import babel from 'rollup-plugin-babel'
+// import minify from 'rollup-plugin-babel-minify'
+
+import resolve from 'rollup-plugin-node-resolve'
+module.exports = [
+
+  {
+    input: './components/wattle-app/wattle-app.js',
+    output: {
+      file: 'distr/wattle-app.js', // IIFE ONE FILE
+      format: 'esm'
+    },
+    plugins: [resolve({})]
+  }
+]
+
